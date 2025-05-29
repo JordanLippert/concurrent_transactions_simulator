67a58d631b66ac5160078b222fa21e727c10b302
JordanLippert <lippertx11@gmail.com>
2025-05-28 23:52:17 -0300

    feat: criacao e implementacao do utils para exibir as mensagens e simular atrasos aleatorios

---
+++ src/utils.py
@@ -0,0 +1,22 @@
+import time
+import random
+from colorama import Fore, Style
+
+def delay(min_time=0.1, max_time=0.5):
+    """Simula um tempo de execução aleatório entre as etapas."""
+    time.sleep(random.uniform(min_time, max_time))
+
+def log_info(message):
+    print(f"{Fore.CYAN}[INFO] {message}{Style.RESET_ALL}")
+
+def log_sucess(message):
+    print(f"{Fore.GREEN}[OK] {message}{Style.RESET_ALL}")
+
+def log_warning(message):
+    print(f"{Fore.YELLOW}[AGUARDE] {message}{Style.RESET_ALL}")
+
+def log_error(message):
+    print(f"{Fore.RED}[ERRO] {message}{Style.RESET_ALL}")
+
+def log_critical(message):
+    print(f"{Fore.MAGENTA}[DEADLOCK] {message}{Style.RESET_ALL}")
